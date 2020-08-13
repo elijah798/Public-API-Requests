aaf2dde1e5fda7d4bc990ca2e27e6514cb66b98d
elijah798 <46266593+elijah798@users.noreply.github.com>
2020-08-13 16:50:50 -0500

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 const gallery = document.getElementById('gallery');
 const cards = document.getElementsByClassName('card');
 
+//function to add reusable fetch request
 function fetchData(url) {
     return fetch(url)
         .then(checkStatus)
         .then(res => res.json())
         .catch(error => console.log(error))
 }
-
+//function to check status of the request
 function checkStatus(response) {
     if (response.ok) {
         return Promise.resolve(response);
@@ -16,16 +17,21 @@ function checkStatus(response) {
     }
 }
 
+//public api call to randomUSer
 fetchData('https://randomuser.me/api/?results=12')
     .then(data => {
-        console.log(data.results[0]);
+
+        //for loop to add each retrieved user to a card and modal
         for (var i = 0; i < data.results.length; i++) {
+            //variables indicated personal information of retrieved users
             const person = data.results[i];
             const picture = person.picture.large;
             const name = person.name;
             const email = person.email;
             const location = person.location;
+            //variable to add date to users DOB
             var date = new Date(person.dob.date);
+            //modal template with user information.
             const modal = `<div class="modal-container" style="display:none;" id="${name.first}_${name.last}">
             <div class="modal"">
                 <button type="button" id="modal-close-btn" class="modal-close-btn" onclick='this.parentNode.parentNode.style.display = "none"' ><strong>X</strong></button>
@@ -41,7 +47,7 @@ fetchData('https://randomuser.me/api/?results=12')
                 </div>
             </div>
             </div>`
-
+            //html template for cards to display users
             const html = `
         <div class="card cards" id='${name.first}_${name.last}'>
         <div class="card-img-container cards">
@@ -57,20 +63,18 @@ fetchData('https://randomuser.me/api/?results=12')
             
             
     `;
-
-
-
+    //adds the modals and cards to index.html
             gallery.innerHTML += html;
             document.getElementById('modals').innerHTML += modal;
         }
     })
     .then(check =>{
-
+//checks to see if a card was clicked on to display modal for that specific person
 gallery.addEventListener('click', (e) => {
     if(e.target.classList.contains('cards')){
-       
+    //checks to see if the user clicked on a card and nothing else.
+    //queryselector that grabs the specific ID of the card to dispaly the modal.
     const selected = e.target.parentNode.querySelector(".card-name").textContent.split(" ").join("_")
-    
     let select = document.querySelectorAll(`#${selected}`);
     select[1].style.display = '';
     }
